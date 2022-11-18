d541e610bd38b62ba749dd2e569eb36485d1548c
Daniv112 <2.61.69@mail.ru>
2022-11-18 22:04:03 +0300

    1
    1

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using YG;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] public UnityEvent soundOn;
+    [SerializeField] public UnityEvent soundOff;
+    public static SoundManager instance;
+
+    private void OnEnable() => YandexGame.GetDataEvent += GetLoad;
+    private void OnDisable() => YandexGame.GetDataEvent -= GetLoad;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance == this)
+            Destroy(gameObject);
+       // DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        if (YandexGame.SDKEnabled == true)
+        {
+            GetLoad();
+        }
+    }
+
+    private void GetLoad() 
+    {
+        bool enable = YandexGame.savesData.sound;
+        if (enable)
+        {
+            soundOn.Invoke();
+        }
+        else
+        {
+            soundOff.Invoke();
+        }
+    }
+
+    public void SetSound(bool enable) 
+    {
+        YandexGame.savesData.sound = enable;
+        YandexGame.SaveProgress();
+        if (enable)
+        {
+            soundOn.Invoke();
+        }
+        else
+        {
+            soundOff.Invoke();
+        }
+    }
+}
--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -7,8 +7,6 @@ using YG;
 
 public class Menu : MonoBehaviour
 {
-    [SerializeField] private Button soundOn;
-    [SerializeField] private Button soundOff;
     [SerializeField] private Text allGoldText;
     // Подписываемся на событие GetDataEvent в OnEnable
     private void OnEnable() => YandexGame.GetDataEvent += GetLoad;
@@ -34,19 +32,6 @@ public class Menu : MonoBehaviour
     {
         if (allGoldText != null)
             allGoldText.text = YandexGame.savesData.gold.ToString();
-        SwapButtonSound(YandexGame.savesData.sound);
-    }
-    public void SetSound(bool enable)
-    {
-        YandexGame.savesData.sound = enable;
-        YandexGame.SaveProgress();
-        SwapButtonSound(enable);
-    }
-
-    private void SwapButtonSound(bool sound)
-    {
-        soundOff.gameObject.SetActive(!sound);
-        soundOn.gameObject.SetActive(sound);
     }
 
     public void LoadScene(int scene)
