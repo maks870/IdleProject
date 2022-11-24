b0e90ee940890d75d121e1e1da2ca6e47f463edb
maks870 <maks7-2014@inbox.ru>
2022-11-24 16:05:14 +0300

    1

---
+++ Assets/Scripts/Characters/Enemy/UndeadEnemy.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UndeadEnemy : Enemy
+{
+    [SerializeField] private bool isDead = false;
+    [SerializeField] private bool isDyingProcess = false;
+    private Collider2D boxCollider;
+    private int baseHp;
+    private float baseSpeed;
+
+    protected override void Start()
+    {
+        base.Start();
+        baseHp = hp;
+        baseSpeed = speed;
+        boxCollider = GetComponent<Collider2D>();
+    }
+    protected override void Update()
+    {
+        base.Update();
+        if (isDyingProcess)
+            speed = 0;
+        else
+            speed = baseSpeed;
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        if (!isDyingProcess)
+            base.TakeDamage(damage);
+    }
+
+    protected override void Dead()
+    {
+        if (!isDead)
+            StartCoroutine(DyingProcess(actionCooldown));
+        else
+            base.Dead();
+    }
+
+    IEnumerator DyingProcess(float time)
+    {
+        isDyingProcess = true;
+        boxCollider.isTrigger = true;
+        Color color = spriteRenderer.color;
+        spriteRenderer.color = new Color(255, 181, 0, 255);
+
+        yield return new WaitForSeconds(time);
+
+        spriteRenderer.color = color;
+        hp = baseHp;
+        boxCollider.isTrigger = false;
+        isDyingProcess = false;
+        isDead = true;
+    }
+}
