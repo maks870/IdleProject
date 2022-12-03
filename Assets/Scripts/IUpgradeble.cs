b70efd109fe20d36486bc582504cdb693952a38f
maks870 <maks7-2014@inbox.ru>
2022-12-03 11:13:11 +0300

    1

--- Assets/Scripts/IUpgradeble.cs
+++ Assets/Scripts/IUpgradeble.cs
@@ -1,7 +1,7 @@
 public interface IUpgradeble
 {
+    public void SetDataVariables();
     public int GetStatLvl(string statName);
     public void Upgrade(string statName);
-    public void SetDataVariables();
 
 }
--- Assets/Scripts/Magnet.cs
+++ Assets/Scripts/Magnet.cs
@@ -1,16 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using YG;
 
-public class Magnet : MonoBehaviour
+public class Magnet : MonoBehaviour, IUpgradeble
 {
-    public float speed;
+    [SerializeField] private float speed;
+    private CircleCollider2D col;
+    private void Awake()
+    {
+        col = GetComponent<CircleCollider2D>();
+        SetDataVariables();
+    }
+
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.tag == "Magnetized") 
+        if (collision.tag == "Magnetized")
         {
-            Vector3 direct =Player.instance.transform.position - collision.transform.position;
+            Vector3 direct = Player.instance.transform.position - collision.transform.position;
             collision.transform.position += direct.normalized * Time.deltaTime * speed;
         }
     }
+    public void SetDataVariables()
+    {
+        Upgrader upgrader = GetComponent<Upgrader>();
+        col.radius = (upgrader.GetBaseValue("collectorRadius") * upgrader.GetValue("collectorRadius") / 100);
+    }
+    public void Upgrade(string statName)
+    {
+        YandexGame.savesData.collector.AddValue(statName, 1);
+    }
+    public int GetStatLvl(string statName)
+    {
+        return YandexGame.savesData.collector.GetValue(statName);
+    }
+
 }
